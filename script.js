0815632aed10b45493aacd2f1072474eaaa3d28a
Catalina Simonovich <catalinasimonovich1@gmail.com>
2022-10-12 22:52:37 -0300

    :tada:Feat: Create a new repository with a training JS exercise

---
+++ script.js
@@ -0,0 +1,34 @@
+var name = 'Catalina';
+var age = 22;
+var single = false;
+var numbers = [1,2,3,4,5,6,7,8,9,10];
+var colleagues = ['Andy', 'Rochius', 'Leo. C', 'Cata', 'Isa']
+var values = {
+    name: 'Catalina',
+    age: 22,
+    single: false,
+    numbers: [1,2,3,4,5,6,7,8,9,10],
+    colleagues: ['Andy','Rochius','Leo. C','Cata','Isa']
+}
+var user = {
+    name: 'Leonardo',
+    lastname: 'Cipollone',
+    age: 25,
+    favnumbers: [7,4,25],
+    pets: {
+        pet1: {
+            name: 'Tomi',
+            animal: 'dog',
+            age: 4,
+            color: 'black'
+        },
+        pet2: {
+            name: 'Chiquito',
+            animal: 'dog',
+            age: 0.2,
+            color: 'white'
+        }
+    }
+}
+
+console.log(name, age, single, numbers, colleagues, values, user);
